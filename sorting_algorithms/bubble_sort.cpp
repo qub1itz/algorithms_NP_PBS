68c6663fee81b89ee2adfc63e7fe0ca4de68c3e7
Wiktor Błaszkiewicz <73906310+qub1itz@users.noreply.github.com>
2023-01-22 12:36:08 +0100

    Create bubble_sort.cpp

---
+++ sorting_algorithms/bubble_sort.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+void bubble_sort(int tab[], int n){
+  cout<<"Posortowana tablica: "<<endl;
+  for(int i=0; i<n; i++){
+    for(int j=1; j<n; j++){
+      if(tab[j]>tab[j-1]){
+        swap(tab[j],tab[j-1]);
+      }
+    }
+  }
+  for(int k=0; k<n; k++){
+    cout<<tab[k]<<"\t";
+  }
+}
+int main(){
+  
+  return 0;
+}
