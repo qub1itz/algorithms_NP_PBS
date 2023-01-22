f1b2c94a381c9df7afc371ef4613b7a3a4c3a40f
Wiktor Błaszkiewicz <73906310+qub1itz@users.noreply.github.com>
2023-01-22 14:01:48 +0100

    Update bubble_sort.cpp

--- sorting_algorithms/bubble_sort.cpp
+++ sorting_algorithms/bubble_sort.cpp
@@ -1,14 +1,10 @@
 #include <iostream>
 using namespace std;
 void bubble_sort(int tab[], int n){
-  cout<<"Sorted numbers: "<<endl;
+  cout<<"Posortowane liczby: "<<endl;
   for(int i=0; i<n; i++){
     for(int j=1; j<n; j++){
       if(tab[j]>tab[j-1]){
-        swap(tab[j],tab[j-1]);
-      }
-    }
-  }
   
   for(int k=0; k<n; k++){
     cout<<tab[k]<<"\t";
