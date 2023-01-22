34d44c2004ef2a3f0a62dff9005ac553f2a3927e
Hubert Filarecki <122351501+Tygras@users.noreply.github.com>
2023-01-22 13:22:30 +0100

    Update bubble_sort.cpp

--- sorting_algorithms/bubble_sort.cpp
+++ sorting_algorithms/bubble_sort.cpp
@@ -1,20 +1,28 @@
 #include <iostream>
+
 using namespace std;
-void bubble_sort(int tab[], int n){
-  cout<<"Posortowane liczby: "<<endl;
-  for(int i=0; i<n; i++){
-    for(int j=1; j<n; j++){
-      if(tab[j]>tab[j-1]){
+
+void bubble_sort(int tab[], int n)
+{
+  for(int i=0; i<n; i++)
+  {
+    for(int j=1; j<n; j++)
+    {
+      if(tab[j]>tab[j-1])
+      {
         swap(tab[j],tab[j-1]);
       }
     }
   }
   
-  for(int k=0; k<n; k++){
+  cout<<"Posortowane liczby: "<<endl;
+  for(int k=0; k<n; k++) //wypisanie posortowanych liczb
+  {
     cout<<tab[k]<<"\t";
   }
 }
-int main(){
-  
+
+int main()
+{
   return 0;
 }
