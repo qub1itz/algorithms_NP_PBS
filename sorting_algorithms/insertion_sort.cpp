edaf33049c7796dc854e7603eeff732caae63eb1
Tygras <122351501+Tygras@users.noreply.github.com>
2023-01-22 12:53:47 +0100

    Add files via upload

---
+++ sorting_algorithms/insertion_sort.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <fstream>
+#include <bits/stdc++.h>
+#include <chrono>
+#include <ctime>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    int n=50;   //tutaj wybieramy ile liczb z wybranego pliku chcemy posortowac
+    int tab[n];
+    int element;
+
+    cout << "Liczby przed sortowaniem: " << endl; //wypisanie liczb przed sortowaniem
+    for (int i=0;i<n;i++)
+    {
+        cout<<tab[i]<<" ";
+    }
+
+    cout << " " << endl;
+    cout << " " << endl;
+
+    //ALGORYTM SORTOWANIA PRZEZ WSTAWIANIE
+    int pomoc, j;
+    for(int i=0; i<n; i++)
+    {
+        pomoc = tab[i];
+        j = i-1;
+        while(j>=0 && tab[j]>pomoc)
+        {
+            tab[j+1] = tab[j];
+            j--;
+        }
+        tab[j+1] = pomoc;
+    }
+    //KONIEC ALGORYTMU
+
+    cout << "Liczby po sortowaniu: " << endl;
+
+    for(int i=0; i<n; i++)  //wypisanie liczb posortowanych
+    {
+        cout <<tab[i]<<" ";
+    }
+
+    return 0;
+}
